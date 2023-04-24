a7c92791d9ea3a08d1fbf0cffdd79bf7de514bbf
William Harrison <william@williamdavidharrison.com.au>
2023-04-24 08:07:16 +0800

    upload

---
+++ ytdl.py
@@ -0,0 +1,28 @@
+import os, pytube, sys, uuid
+
+url = input("Please enter a YouTube video URL:\n>> ")
+
+yt = pytube.YouTube(url)
+
+print()
+print("------- Video -------")
+print("Channel:", yt.author)
+print("Title:", yt.title)
+print("Length (seconds):", yt.length)
+print("Uploaded:", yt.publish_date)
+print("---------------------")
+print()
+
+video = yt.streams.get_highest_resolution()
+name = str(uuid.uuid4()) + ".mp4"
+
+print("Downloading video...")
+
+video.download("./", name)
+
+print("\nDownloaded video!")
+print("File Location:", os.getcwd() + "\\" + name)
+
+print()
+os.system("pause")
+sys.exit()
